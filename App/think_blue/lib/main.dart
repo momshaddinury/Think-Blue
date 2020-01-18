614a57beedc38309583906e645ff9d38493f81d3
Momshad Dinury <tawsifshenron@gmail.com>
2020-01-18 12:33:56 +0600

    Widget Overflow Issue #2

--- App/think_blue/lib/main.dart
+++ App/think_blue/lib/main.dart
@@ -22,17 +22,13 @@ class BodyFunction extends StatelessWidget {
     return SafeArea(
       child: Column(
         children: <Widget>[
-          Container(
-            //Center Image
-            padding: EdgeInsets.only(top: 100.0, bottom: 50),
-            child: Image.asset(
-              'images/waterhd.png',
-              scale: 0.8,
-            ),
-          ),
-          Column(
-            //App Name Holder
+          Expanded(
+            flex: 4,
+            child: Container(        //Center Image
+              child: Image.asset('images/waterhd.png'))),
+          Column(                   //App Name Holder
             crossAxisAlignment: CrossAxisAlignment.end,
+            //mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Container(
                 child: Text(
@@ -60,42 +56,40 @@ class BodyFunction extends StatelessWidget {
               ),
             ],
           ),
-          Container(
-            width: double.infinity,
-          ),
           SizedBox(
             height: 50.0,
           ),
-          Container(
-            width: 420,
-            color: Colors.white,
-            height: 103,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                ButtonTheme(
-                  minWidth: 260.0,
-                  height: 55.0,
-                  child: RaisedButton(
-                    shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(18.0),
-                      side: BorderSide(color: Colors.blue.shade900),
-                    ),
-                    onPressed: () {},
-                    splashColor: Colors.blueAccent,
-                    color: Colors.blue.shade900,
-                    textColor: Colors.white,
-                    child: Text("Start now".toUpperCase(),
-                        style: TextStyle(
-                          fontSize: 26,
-                          fontFamily: 'Roboto',
-                          letterSpacing: 0.5,
-                        )
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  ButtonTheme(
+                    minWidth: 260.0,
+                    height: 55.0,
+                    child: RaisedButton(
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.blue.shade900),
+                      ),
+                      onPressed: () {},
+                      splashColor: Colors.blueAccent,
+                      color: Colors.blue.shade900,
+                      textColor: Colors.white,
+                      child: Text("Start now".toUpperCase(),
+                          style: TextStyle(
+                            fontSize: 26,
+                            fontFamily: 'Roboto',
+                            letterSpacing: 0.5,
+                          )
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
