35588cec53978bc09d8813aac641d20aa6c38fc6
Momshad Dinury <tawsifshenron@gmail.com>
2020-01-31 19:09:31 +0600

    Modularized Code / Sign up page - in progress

--- App/think_blue/lib/main.dart
+++ App/think_blue/lib/main.dart
@@ -1,15 +1,18 @@
-import 'dart:ui';
-
+import 'welcome_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(BaseFunction());
 
 class BaseFunction extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.blue, //or set color with: Color(0xFF0000FF)
+    ));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Landing Page',
+      title: 'THINK blue',
       home: Scaffold(
         backgroundColor: Colors.white,
         body: WelcomeScreen(),
@@ -18,109 +21,4 @@ class BaseFunction extends StatelessWidget {
   }
 }
 
-class WelcomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            flex:5,
-            child: Container(
-              height: double.maxFinite,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                  color: Colors.blue,
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20.0),
-                      bottomRight: Radius.circular(20.0)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 10.0,
-                    ),
-                  ]),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Image.asset('images/waterhd.png'),
-                  Column(
-                    //App Name Holder
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Container(
-                        child: Text("THINK",
-                            style: TextStyle(
-                                color: Colors.blue.shade900,
-                                fontWeight: FontWeight.w800,
-                                fontFamily: 'Roboto',
-                                letterSpacing: 0.5,
-                                fontSize: 65)),
-                      ),
-                      Container(
-                        child: Text("blue",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w800,
-                                fontFamily: 'Roboto',
-                                letterSpacing: 0.5,
-                                fontSize: 65)),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 25.0),
-              child: RaisedButton(
-                elevation: 7,
-                shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(10.0),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SecondRoute()),
-                  );
-                },
-                splashColor: Colors.blueAccent,
-                color: Colors.blue.shade900,
-                child: Text("Start now".toUpperCase(),
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'Roboto',
-                        letterSpacing: 0.5,
-                        color: Colors.white)),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
 
-class SecondRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Route"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
-      ),
-    );
-  }
-}
